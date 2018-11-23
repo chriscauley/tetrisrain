f9ca0e9cfb823aac4c54eb9994edb139d8d7a76a
chriscauley <chris@lablackey.com>
2018-11-23 18:47:12 +0000

    adding ur ready to unrest

--- js/unrest.js/index.js
+++ js/unrest.js/index.js
@@ -1,3 +1,9 @@
 import uR from './Object'
 
+import Ready from "./ready";
+
+uR.ready = Ready();
+window.onload = uR.ready.start;
+window.uR = uR
+
 export default uR
---
+++ js/unrest.js/ready.js
@@ -0,0 +1,28 @@
+/* A task queue that can be started and stopped.
+   Usage:
+   ready = new Ready();
+   const f = () => console.log(1);
+   ready(f);
+   ready(f,f,f);
+   ready.start(); // logs 1 four times
+   ready(f) // logs 1
+   ready.stop();
+   ready(f); // no logs (until next start)
+*/
+
+const Ready = (isReady=()=>false,_ready=[]) => {
+  const ready = (...args) => {
+    _ready = _ready.concat(args);
+    while (isReady() && _ready.length) { _ready.shift()(); }
+  };
+  ready.start = function() {
+    isReady = () => true;
+    ready();
+  };
+  ready.stop = function() {
+    isReady = () => false;
+  };
+  return ready;
+};
+
+export default Ready;
