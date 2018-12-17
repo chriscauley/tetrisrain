118eae3a7128beb70e417699e6e2f0e8514c4a3e
chriscauley <chris@lablackey.com>
2018-12-17 16:44:36 +0000

    cleaning up index a bit

--- js/unrest.js/random/index.js
+++ js/unrest.js/random/index.js
@@ -1,9 +1,15 @@
-// This provides a wrapper around Math.random for a few common functions, as well as an interface to tap into a seeded RNG in order to run unit tests on anything using randomness.
+/* Usage:
+   random = Random(SEED) // seed is an integer or string
+   random() // float 0-1
+   random.int(N) // int [0,N-1]
+   random.int() // int [0,2147483646]
+   random.choice(array) // random element from array
+   random.shuffle(array) // Returns shuffled array (mutates array)
+   random.reset() // returns PRNG to begining
+*/
 
-// PRNG borrowed from https://gist.github.com/blixt/f17b47c62508be59987b
+// PRNG algorithm borrowed from https://gist.github.com/blixt/f17b47c62508be59987b
 export default (seed) => {
-  var _seed;
-
   // https://stackoverflow.com/a/7616484
   if (typeof seed == "string") { // convert string to integer
     var res = 0, len = seed.length;
@@ -12,50 +18,49 @@ export default (seed) => {
       res = res & res;
     }
     seed = res;
+  } else if (isNaN(seed)) {
+    // seed was neither string or number... pick a truely random seed
+    seed = Math.floor(Math.random()*2147483647);
   }
 
-  // #! TODO see _analyzeRandom
-  // uR._random_seeds[seed] = (uR._random_seeds[seed] || 0) + 1;
+  let _current;
   const random = () => (random.raw() - 1) / 2147483646; // 0-1
 
   Object.assign(random,{
-    int: (min,max) => {
+    seed: seed,
+    int: (min=2147483647,max) => {
       // min-max or 0-min if no max
-      return (max === undefined)?random.int(0,min):Math.floor(random()*(max-min)+min);
+      if (max === undefined) {
+        max = min
+        min = 0
+      }
+      return Math.floor(random()*(max-min)+min);
     },
-    raw: () => _seed = _seed * 16807 % 2147483647, // 0-2147483646
+    raw: () => _current = _current * 16807 % 2147483647, // 0-2147483646
     choice: (array) => array[random.int(array.length)],
     reset: () =>  {
-      _seed = seed % 2147483647;
-      if (_seed <= 0) _seed += 2147483646;
+      _current = seed % 2147483647;
+      if (_current <= 0) _current += 2147483646;
     },
-    seed: seed,
-    getSeed: () => _seed,
-    getNextSeed: () => {
-      return random.raw()%8191;// 2^13-1...because why not?
+    _getCurrent: () => _current,
+    shuffle: (array) => {
+      var i = array.length, temp, i_rand;
+      // While there remain elements to shuffle...
+      while (0 !== i) {
+        // Pick a remaining element...
+        i_rand = Math.floor(random() * i);
+        i -= 1;
+        // And swap it with the current element.
+        temp = array[i];
+        array[i] = array[i_rand];
+        array[i_rand] = temp;
+      }
+      return array;
     },
   })
   random.reset();
-  if (isNaN(_seed)) { // seed was neither string or number... pick a truely random seed
-    random.raw = () => Math.floor(Math.random()*2147483647);
-  }
 
-  random.shuffle = (array) => {
-    var i = array.length, temp, i_rand;
-    // While there remain elements to shuffle...
-    while (0 !== i) {
-      // Pick a remaining element...
-      i_rand = Math.floor(random() * i);
-      i -= 1;
-      // And swap it with the current element.
-      temp = array[i];
-      array[i] = array[i_rand];
-      array[i_rand] = temp;
-    }
-    return array;
-  };
-
-  // the first number is always ~ 0.01, so let's burn that one
+  // for SEED < 10,000 the first number is always ~ 0.01, so let's burn that one
   random()
   return random;
 };
@@ -64,8 +69,13 @@ export const RandomMixin = superclass => class Random extends superclass {
   // creates a method this.random which is a PRNG based on opts._SEED or opts.parent.random
   constructor(opts={}) {
     super(opts);
-    this._SEED = opts._SEED || opts.seed;
-    if (opts._prng) { this._SEED = opts._prng.random.getNextSeed(); }
+    if (opts._prng) {
+      // derive seed from a parent PRNG
+      this._SEED = opts._prng.random.getNextSeed();
+    } else {
+      this._SEED = opts._SEED || opts.seed;
+    }
+
     this.random = new uR.Random(this._SEED);
   }
 };
