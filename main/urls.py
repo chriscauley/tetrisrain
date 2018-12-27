094e0d4f5e69e9baf0a87a367ad9df75fdd416d3
chriscauley <chris@lablackey.com>
2018-12-27 22:29:44 +0000

    adding nopass and user.json urls

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 import unrest.views
+from unrest.nopass.views import create as nopass_create
 
 urlpatterns = [
+    path("", unrest.views.redirect,kwargs={'url':'/static/index.html'}),
     path('admin/', admin.site.urls),
     path('api/nopass/',include('unrest.nopass.urls')),
     re_path('api/(main).(Game|Replay)/$', unrest.views.list_view),
+    path("user.json",unrest.views.user_json),
+    path("api/auth/register/",nopass_create),
 ]
-
